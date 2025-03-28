4054244a90dcb5c153f3c90a7935e9db2ef4d0da
Cthv9 <service.davidesaim@gmail.com>
2025-03-28 15:55:24 +0100

    v2

--- service-worker.js
+++ service-worker.js
@@ -13,6 +13,7 @@ const urlsToCache = [
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
@@ -32,7 +33,7 @@ self.addEventListener('activate', function(event) {
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (!cacheWhitelist.includes(cacheName)) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
