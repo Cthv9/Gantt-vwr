d04982a577db4864b06540f56efdc12dfe639e60
Cthv9 <service.davidesaim@gmail.com>
2025-03-28 14:49:05 +0100

    v1.1

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,13 @@
 const CACHE_NAME = 'gantt-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
+  '/Gantt-vwr/',
+  '/Gantt-vwr/index.html',
+  '/Gantt-vwr/manifest.json',
+  '/Gantt-vwr/service-worker.js',
   'https://cdnjs.cloudflare.com/ajax/libs/frappe-gantt/0.5.0/frappe-gantt.css',
   'https://cdnjs.cloudflare.com/ajax/libs/frappe-gantt/0.5.0/frappe-gantt.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js'
-  // Aggiungi altre risorse se necessario
 ];
 
 self.addEventListener('install', function(event) {
